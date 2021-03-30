f27311d1643c0c68e21109dca266d8f56fb0b391
Evan Tremarche <emtremarche@comcast.net>
2021-03-29 20:03:19 -0400

    Add project files.

---
+++ gitreview1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace gitreview1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string today = "Today is Tuesday, February 9, 2021";
+            Console.WriteLine(today);
+
+            Console.ReadKey();
+        }
+    }
+}
